32d568b1f06b5eece2ebbd1f4813e7a7617c6981
Szymon Papuga <szymek.p43@gmail.com>
2017-04-12 17:01:47 +0200

    new project

---
+++ src/main/java/com/bachelor/MySpotifyApplication.java
@@ -0,0 +1,12 @@
+package com.bachelor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpotifyApplication.class, args);
+	}
+}
